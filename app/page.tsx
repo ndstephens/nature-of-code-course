a21c8a1116bbcd4d5cfbd2a5ad0e32d3508c94a6
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-05-05 16:48:23 -0400

    rename directory from mouseMove to mouse-move

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { type Metadata } from 'next';
 
 import LearnP5Sketch from 'components/learn-p5/LearnP5Sketch';
-import MouseMoveSketch from 'components/learn-p5/mouseMove/MouseMove';
 
+// import MouseMoveSketch from 'components/learn-p5/mouse-move/MouseMove';
 // import TestSketch from 'components/test-sketch/TestSketch';
 
 export const metadata: Metadata = {
@@ -13,8 +13,8 @@ export default function HomePage() {
   return (
     <main className="flex grow flex-col items-center justify-center">
       {/* <TestSketch /> */}
-      <MouseMoveSketch />
-      {/* <LearnP5Sketch /> */}
+      {/* <MouseMoveSketch /> */}
+      <LearnP5Sketch />
     </main>
   );
 }
